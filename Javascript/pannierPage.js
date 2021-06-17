811f079cc893f14b8452b234fc014fcd7c20ea65
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-06-17 18:36:48 +0200

    ajffichage du pannier (bug d'affichage)

--- Javascript/pannierPage.js
+++ Javascript/pannierPage.js
@@ -3,11 +3,17 @@ main()
 function main()
 {
     for (const teddie of localStorage.pannier) {
-        
+        displayPannier(teddie)
+        console.log(teddie)
     }
 }
 
-function displayPannier()
+function displayPannier(_teddie)
 {
+    let template = document.getElementById("articlePanier");
+    let cloneElt = document.importNode(template.content, true)
 
+    cloneElt.getElementById("pannier__teddies__text__name").textContent = _teddie.name
+
+    document.getElementById("pannier").appendChild(cloneElt)
 }
